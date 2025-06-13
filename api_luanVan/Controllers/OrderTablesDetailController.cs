c3aa150aa1476cb0c1cb78c54c0ee2c3b29333ed
trinhtu34 <trinhngoctu007@gmail.com>
2025-06-13 10:54:40 +0700

    fix ordertableDetail

--- api_luanVan/Controllers/OrderTablesDetailController.cs
+++ api_luanVan/Controllers/OrderTablesDetailController.cs
@@ -1,6 +1,8 @@
-﻿using api_LuanVan.Models;
+﻿using api_LuanVan.DataTransferObject;
+using api_LuanVan.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_LuanVan.Controllers
 {
@@ -14,5 +16,59 @@ namespace api_LuanVan.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DTO_OrderTablesDetail>>> GetAllOrderTablesDetails()
+        {
+            return await _context.OrderTablesDetails
+                .Select(otd => new DTO_OrderTablesDetail
+                {
+                    OrderTablesDetailsId = otd.OrderTablesDetailsId,
+                    OrderTableId = otd.OrderTableId,
+                    TableId = otd.TableId
+                })
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DTO_OrderTablesDetail>> GetOrderTablesDetailById(int id)
+        {
+            var orderTableDetail = await _context.OrderTablesDetails.FindAsync(id);
+            if (orderTableDetail == null)
+                return NotFound();
+            return new DTO_OrderTablesDetail
+            {
+                OrderTablesDetailsId = orderTableDetail.OrderTablesDetailsId,
+                OrderTableId = orderTableDetail.OrderTableId,
+                TableId = orderTableDetail.TableId
+            };
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DTO_OrderTablesDetail>> CreateOrderTablesDetail([FromBody] DTO_OrderTablesDetail dto)
+        {
+            var orderTableDetail = new OrderTablesDetail
+            {
+                OrderTableId = dto.OrderTableId,
+                TableId = dto.TableId
+            };
+            _context.OrderTablesDetails.Add(orderTableDetail);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetOrderTablesDetailById), new { id = orderTableDetail.OrderTablesDetailsId }, dto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrderTablesDetail(int id, [FromBody] DTO_OrderTablesDetail dto)
+        {
+            if (id != dto.OrderTablesDetailsId)
+                return BadRequest();
+            var orderTableDetail = await _context.OrderTablesDetails.FindAsync(id);
+            if (orderTableDetail == null)
+                return NotFound();
+            orderTableDetail.OrderTableId = dto.OrderTableId;
+            orderTableDetail.TableId = dto.TableId;
+            _context.Entry(orderTableDetail).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
