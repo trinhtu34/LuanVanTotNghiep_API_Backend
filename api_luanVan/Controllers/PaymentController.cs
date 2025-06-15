db07494d9ba87bc752bfdd7f1de8d3f44fd16e34
trinhtu34 <trinhngoctu007@gmail.com>
2025-06-15 14:57:55 +0700

    add payment feature

--- api_luanVan/Controllers/PaymentController.cs
+++ api_luanVan/Controllers/PaymentController.cs
@@ -37,5 +37,51 @@ namespace api_LuanVan.Controllers
                     TransactionStatusDescription = pr.TransactionStatusDescription
                 }).ToListAsync();
         }
+        [HttpGet("ordertable/{id}")]
+        public async Task<ActionResult<IEnumerable<DTO_Payment>>> GetPaymentResultsByOrderTableId(long id)
+        {
+            var paymentResults = await _context.PaymentResults
+                .Where(pr => pr.OrderTableId == id)
+                .Select(pr => new DTO_Payment
+                {
+                    PaymentResultId = pr.PaymentResultId,
+                    OrderTableId = pr.OrderTableId,
+                    PaymentId = pr.PaymentId,
+                    IsSuccess = pr.IsSuccess,
+                    Description = pr.Description,
+                    Timestamp = pr.Timestamp,
+                    VnpayTransactionId = pr.VnpayTransactionId,
+                    PaymentMethod = pr.PaymentMethod,
+                    BankCode = pr.BankCode,
+                    BankTransactionId = pr.BankTransactionId,
+                    ResponseDescription = pr.ResponseDescription,
+                    TransactionStatusDescription = pr.TransactionStatusDescription
+                }).ToListAsync();
+            if (paymentResults == null || !paymentResults.Any())
+                return NotFound();
+            return paymentResults;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DTO_Payment>> CreatePaymentResult(DTO_Payment paymentDto)
+        {
+            var paymentResult = new PaymentResult
+            {
+                OrderTableId = paymentDto.OrderTableId,
+                PaymentId = paymentDto.PaymentId,
+                IsSuccess = paymentDto.IsSuccess,
+                Description = paymentDto.Description,
+                Timestamp = paymentDto.Timestamp,
+                VnpayTransactionId = paymentDto.VnpayTransactionId,
+                PaymentMethod = paymentDto.PaymentMethod,
+                BankCode = paymentDto.BankCode,
+                BankTransactionId = paymentDto.BankTransactionId,
+                ResponseDescription = paymentDto.ResponseDescription,
+                TransactionStatusDescription = paymentDto.TransactionStatusDescription
+            };
+            _context.PaymentResults.Add(paymentResult);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetPaymentResultsByOrderTableId), new { id = paymentResult.OrderTableId }, paymentDto);
+        }
     }
 }
