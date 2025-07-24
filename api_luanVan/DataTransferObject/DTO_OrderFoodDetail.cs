aca7ece7397f4b334f2bd5ffa957268cab3752b7
trinhtu34 <trinhngoctu007@gmail.com>
2025-07-24 16:17:29 +0700

    fix bug in order food detail

--- api_luanVan/DataTransferObject/DTO_OrderFoodDetail.cs
+++ api_luanVan/DataTransferObject/DTO_OrderFoodDetail.cs
@@ -6,12 +6,17 @@
 
         public long OrderTableId { get; set; }
 
-        public string DishId { get; set; }
+        public string DishId { get; set; } = null!;
 
         public int Quantity { get; set; }
 
         public decimal Price { get; set; }
 
         public string? Note { get; set; }
+    }
+    public class DTO_OrderFoodDetail_Update_Quantity
+    {
+
+        public int Quantity { get; set; }
     }
 }
