e39302c38ce982a6d3a6d2d584053ecfbb900a90
TuTrinh <trinhngoctu007@gmail.com>
2025-08-03 16:36:38 +0700

    completed create account for guest user to use to order food feature

--- api_luanVan/DataTransferObject/DTO_User.cs
+++ api_luanVan/DataTransferObject/DTO_User.cs
@@ -27,7 +27,8 @@
         public int RolesId { get; set; }
 
         public string? PhoneNumber { get; set; }
-
+        public string? Address { get; set; }
+        public string? Email { get; set; }
         public DateTime? CreateAt { get; set; }
     }
 }
